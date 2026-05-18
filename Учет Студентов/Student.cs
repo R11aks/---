48307b192fbb78e0b9b1a4feb8d518be882c6dd6
R11aks <R11aks@users.noreply.github.com>
2026-05-18 16:06:00 +0300

    Создал класс Student с полями для данных

---
+++ Учет Студентов/Student.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace StudentAppWPF.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string GroupName { get; set; } = string.Empty;
+        public int Course { get; set; }
+        public string SpecialtyName { get; set; } = string.Empty;
+        public string FacultyName { get; set; } = string.Empty;
+        public string StudentCardNumber { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime EnrollmentDate { get; set; }
+        public bool IsStudying { get; set; }
+    }
+}
